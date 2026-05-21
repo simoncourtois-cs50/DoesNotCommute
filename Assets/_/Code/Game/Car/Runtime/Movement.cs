1525946d840dce10a188b3852e19327cfd78efda
Simon Courtois <simon-courtois@hotmail.com>
2026-05-21 22:43:47 +0200

    [Structure]

---
+++ Assets/_/Code/Game/Car/Runtime/Movement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Car.Runtime
+{
+    public class Movement : MonoBehaviour
+    {
+        #region private and protected
+
+        [SerializeField] float _speed;
+        
+
+        #endregion
+    }
+}
